01702ce5698b5fca1c707b60548998cfa4d99fc9
Guus Hoekman <mail@guushoekman.com>
2017-09-26 15:06:26 +0200

    pixal art maker

---
+++ pixel-art-maker/designs.js
@@ -0,0 +1,12 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+  // $("#pixel_canvas")
+};
+
+$("td").click(function() {
+  $(this).css("background-color", $("#colorPicker").val());
+});
