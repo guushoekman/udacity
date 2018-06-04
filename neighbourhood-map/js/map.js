bb582fe3b0736e89640ae056b35ec3dcaf9c0aa1
Guus Hoekman <mail@guushoekman.com>
2018-06-04 12:08:53 +0200

    load leaflet async

--- neighbourhood-map/js/map.js
+++ neighbourhood-map/js/map.js
@@ -1,9 +1,3 @@
-var map = L.map('map').fitBounds([[-33.9245,18.4169],[-33.92955,18.42832]]);
-
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>'
-}).addTo(map);
-
 var markers = [
   {
     title: "Castle of Good Hope",
@@ -42,18 +36,27 @@ var markers = [
   }
 ];
 
-var Place = function(data) {
-  this.title = ko.observable(data.title);
-  this.lat = ko.observable(data.lat);
-  this.lng = ko.observable(data.lng);
-  this.page = ko.observable(data.page);
-  this.marker = ko.observable();
-  this.description = ko.observable(data.description);
-};
+var map;
+function initMap() {
+  map = L.map('map').fitBounds([[-33.9245,18.4169],[-33.92955,18.42832]]);
 
-var controlLayers = {};
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+  }).addTo(map);
+
+  ko.applyBindings(viewModel);
+}
 
 var viewModel = function(){
+  var Place = function(data) {
+    this.title = data.title;
+    this.lat = data.lat;
+    this.lng = data.lng;
+    this.page = data.page;
+    this.description = data.description;
+    this.wiki = "https://en.wikipedia.org/wiki/" + data.page;
+  };
+
   var self = this;
   this.placeList = ko.observableArray([]);
 
@@ -62,38 +65,34 @@ var viewModel = function(){
   });
 
   placeList().forEach(function(location) {
-    var url = "https://en.wikipedia.org/wiki/" + location.page()
     var lGroup = L.layerGroup().addTo(map);
-    var marker = L.marker([location.lat(), location.lng()]).bindPopup("<h2>" + location.title() + "</h2>").addTo(lGroup)
+    var marker = L.marker([location.lat, location.lng]).bindPopup("<h2>" + location.title + "</h2>").addTo(lGroup)
 
     location.marker = marker;
 
     location.marker.on('popupopen', function() {
       $.ajax({
         type: "GET",
-        url: "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + location.page() + "&limit=1&format=json&origin=*",
+        url: "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + location.page + "&limit=1&format=json&origin=*",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function(data, textStatus, jqXHR) {
-          location.marker.getPopup().setContent("<h2>" + location.title() + "</h2><p>" + data[2][0] + "</p>");
+          location.marker.getPopup().setContent("<h2>" + location.title + "</h2><p>" + data[2][0] + "</p><p><a href='" + location.wiki + "'>More on Wikipedia</a>");
+        },
+        error: function() {
+          location.marker.getPopup().setContent("<h2>" + location.title + "</h2><p>Could not load summary, please follow the link below to read more about this location.</p><p><a href='" + location.wiki + "'>More on Wikipedia</a>");
         }
       });
       $(this._icon).addClass("move-marker")
       $(this._shadow).addClass("move-marker")
-      map.setView([location.lat(), location.lng()])
+      map.setView([location.lat, location.lng])
     })
     .on('popupclose', function() {
       $(this._icon).removeClass("move-marker")
       $(this._shadow).removeClass("move-marker")
       map.fitBounds([[-33.9245,18.4169],[-33.92955,18.42832]])
     });
-
-    controlLayers[location.title()] = lGroup;
-    marker.title = location.title();
-    marker.id = location.title();
   });
-
-  showControl = L.control.layers(null, controlLayers, {collapsed: false}).addTo(map);
 
   // Filter markers per user input
 
@@ -109,7 +108,6 @@ var viewModel = function(){
   self.userInput = ko.observable('');
 
   self.filter = function () {
-
     // remove all markers from visible array
     self.visible.removeAll();
     var searchInput = self.userInput().toLowerCase();
@@ -119,7 +117,7 @@ var viewModel = function(){
       map.removeLayer(location.marker);
       // compare user input with title of locations
       // if location title contains user input, show marker
-      if (location.title().toLowerCase().indexOf(searchInput) !== -1) {
+      if (location.title.toLowerCase().indexOf(searchInput) !== -1) {
         self.visible.push(location);
       }
     });
@@ -129,4 +127,6 @@ var viewModel = function(){
   };
 };
 
-ko.applyBindings(viewModel);
+function leafletError() {
+  $("#leaflet-error").show();
+}
